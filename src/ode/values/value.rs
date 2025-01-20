42f7c056f9e345a504de8fb57e510725a88f0dc7
okd <nossie531@gmail.com>
2025-01-21 03:20:15 +0900

    Polish: remove unused items.

--- src/ode/values/value.rs
+++ src/ode/values/value.rs
@@ -12,20 +12,7 @@ pub trait Value:
     + Sub<Self, Output = Self>
     + Mul<f32, Output = Self>
 {
-    /// Returns zero value.
-    fn zero() -> Self {
-        Self::default()
-    }
-
-    /// Sums the elements of an iterator.
-    fn sum<V: Value>(values: impl Iterator<Item = V>) -> V {
-        let mut ret = V::zero();
-        for value in values {
-            ret = ret + value
-        }
-
-        ret
-    }
+    // nop.
 }
 
 impl<T> Value for T
